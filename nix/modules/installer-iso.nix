1ed1567fc0256aa4d549a535cc84016c2ab51bd1
zjarlin <zjarlin@outlook.com>
2026-04-08 13:21:32 +0800

    Fix installer ISO flake evaluation conflicts

--- nix/modules/installer-iso.nix
+++ nix/modules/installer-iso.nix
@@ -11,10 +11,8 @@
     (inputs.nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix")
   ];
 
-  isoImage = {
-    edition = lib.mkDefault "gnome-niri-installer";
-    volumeID = lib.mkDefault "NIRIINSTALL";
-  };
+  isoImage.edition = lib.mkForce "gnome-niri-installer";
+  isoImage.volumeID = lib.mkForce "NIRIINSTALL";
 
   networking.networkmanager.enable = true;
 
@@ -27,23 +25,10 @@
   environment.etc."xdg/autostart/niri-installer-ui.desktop".source =
     (installerUi + "/share/applications/niri-installer-ui.desktop");
 
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "nixos";
-  };
-
-  services.desktopManager.gnome = {
-    enable = true;
-    favoriteAppsOverride = ''
-      [org.gnome.shell]
-      favorite-apps=[ 'niri-installer-ui.desktop', 'org.gnome.Settings.desktop', 'org.gnome.Terminal.desktop', 'firefox.desktop', 'org.gnome.Nautilus.desktop' ]
-    '';
-  };
-
-  services.displayManager.gdm = {
-    enable = true;
-    autoSuspend = false;
-  };
+  services.desktopManager.gnome.favoriteAppsOverride = lib.mkForce ''
+    [org.gnome.shell]
+    favorite-apps=[ 'niri-installer-ui.desktop', 'org.gnome.Settings.desktop', 'org.gnome.Terminal.desktop', 'firefox.desktop', 'org.gnome.Nautilus.desktop' ]
+  '';
 
   system.nixos.tags = [
     "niri-installer"
